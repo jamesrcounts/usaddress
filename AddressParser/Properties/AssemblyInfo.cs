51e809eb0d0a050e0c377e48f39dd11fd7d43306
Jim Counts <jamesrcounts@outlook.com>
2014-03-17 21:21:30 -0700

    Version bump

--- AddressParser/Properties/AssemblyInfo.cs
+++ AddressParser/Properties/AssemblyInfo.cs
@@ -9,4 +9,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("364a8dcc-05a8-4b05-a8e7-9622fdf6f562")]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("0.0.7")]
+[assembly: AssemblyFileVersion("0.0.12")]
