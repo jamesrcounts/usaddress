a5bf839647793d30cc15b16e73b3b11772d3f035
Jim Counts <jamesrcounts@outlook.com>
2017-11-26 07:41:45 -0800

    Update test csproj

--- tests/AddressParser.Tests/Properties/AssemblyInfo.cs
+++ tests/AddressParser.Tests/Properties/AssemblyInfo.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using ApprovalTests.Reporters;
 
 [assembly: UseReporter(typeof(DiffReporter))]
-[assembly: AssemblyTitle("AddressParser.Tests")]
-[assembly: AssemblyDescription("Tests for AddressParser")]
-[assembly: AssemblyCompany("Jim Counts")]
-[assembly: AssemblyProduct("AddressParser.Tests")]
-[assembly: AssemblyCopyright("Copyright © Jim Counts 2014")]
 [assembly: ComVisible(false)]
 [assembly: Guid("09e66d40-173e-4c07-a053-4c0c04cc2c1a")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: CLSCompliant(true)]
