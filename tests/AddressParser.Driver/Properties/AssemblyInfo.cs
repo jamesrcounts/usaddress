d479978127214c7b0f48d2d69e7d2639ecc3f1d2
Jim Counts <jamesrcounts@outlook.com>
2017-11-26 20:31:00 -0800

    Update driver csproj

--- tests/AddressParser.Driver/Properties/AssemblyInfo.cs
+++ tests/AddressParser.Driver/Properties/AssemblyInfo.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("AddressParser.Driver")]
-[assembly: AssemblyDescription("Command line shell for AddressParser")]
-[assembly: AssemblyCompany("Jim Counts")]
-[assembly: AssemblyProduct("AddressParser.Driver")]
-[assembly: AssemblyCopyright("Copyright © Jim Counts 2011")]
 [assembly: ComVisible(false)]
 [assembly: Guid("98192527-de55-4c54-9509-75ef1f2027ac")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
