15eda790b46c6c0c8b5baf0577eb04f676001424
Jim Counts <jamesrcounts@outlook.com>
2017-11-26 20:35:12 -0800

    update library csproj

--- src/AddressParser/Properties/AssemblyInfo.cs
+++ src/AddressParser/Properties/AssemblyInfo.cs
@@ -10,13 +10,8 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("AddressParser")]
 [assembly: AssemblyDescription("This is a partial port of the Perl Geo::StreetAddress::US CPAN module to C#.")]
-[assembly: AssemblyCompany("Jim Counts")]
-[assembly: AssemblyProduct("AddressParser")]
 [assembly: AssemblyCopyright("Copyright © Jim Counts 2011")]
 [assembly: ComVisible(false)]
 [assembly: Guid("364a8dcc-05a8-4b05-a8e7-9622fdf6f562")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("0.0.20")]
 [assembly: CLSCompliant(true)]
