0ae9eb46d2470778288e1cb745fc640351ff41a8
Jim Counts <jamesrcounts@outlook.com>
2017-11-27 19:12:25 -0800

    Move approvals to subdir

--- tests/AddressParser.Tests/Properties/AssemblyInfo.cs
+++ tests/AddressParser.Tests/Properties/AssemblyInfo.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using ApprovalTests.Namers;
 using ApprovalTests.Reporters;
 
 [assembly: UseReporter(typeof(DiffReporter))]
 [assembly: ComVisible(false)]
 [assembly: Guid("09e66d40-173e-4c07-a053-4c0c04cc2c1a")]
 [assembly: CLSCompliant(true)]
+[assembly: UseApprovalSubdirectory("approvals")]
