967e1d46e1f39ad8d10c3512f4c327512dc010ad
Jim Counts <jamesrcounts@outlook.com>
2017-11-27 18:43:35 -0800

    generate package on build

--- src/AddressParser/Properties/AssemblyInfo.cs
+++ src/AddressParser/Properties/AssemblyInfo.cs
@@ -1,17 +1,6 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="Jim Counts">
-//     Copyright (c) Jim Counts 2013.
-// </copyright>
-// <summary>
-//   Assembly-wide configuration
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
-using System;
-using System.Reflection;
+﻿using System;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyDescription("This is a partial port of the Perl Geo::StreetAddress::US CPAN module to C#.")]
-[assembly: AssemblyCopyright("Copyright © Jim Counts 2011")]
 [assembly: ComVisible(false)]
 [assembly: Guid("364a8dcc-05a8-4b05-a8e7-9622fdf6f562")]
 [assembly: CLSCompliant(true)]
